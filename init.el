38934f4260023069ead178b60185f6c88d00664a
demixire <demixire@gmail.com>
2023-01-07 23:00:56 +0300

    Initial commit

---
+++ init.el
@@ -0,0 +1,164 @@
+;;; Emacs init.el file ;;;
+;;; Author: Demixire <demixire@gmail.com>
+;;; Emacs version: GNU Emacs 28.1 and newer
+
+;; Start Emacs server if not already running.
+(if (and (fboundp 'server-running-p)
+         (not (server-running-p)))
+    (server-start))
+
+;; Speed up Emacs start up time.
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
+
+;; Set packages 
+(require 'package)
+(setq 'package-archives '(("melpa" . "http://melpa.org/packages/")
+				 ("melpa-stable" . "http://stable.melpa.org/packages/")
+				 ("gnu" . "http://elpa.gnu.org/packages/")))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(package-initialize)
+
+;; Load other packages 
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+
+;; Load theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'brutal-dark t)
+
+;; Stop creating backup and autosave files.
+(setq make-backup-files nil) 
+(setq auto-save-default nil) 
+
+
+;; Visual Appearance
+;; Disable tool-bar, menu-bar, scroll-bar
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; Enable highlight-line-mode, display-line-numbers
+(global-hl-line-mode 1)
+(global-display-line-numbers-mode 1)
+
+;; Remove startup-message
+(setq inhibit-startup-message t)
+
+;; Ignore ring bell
+(setq ring-bell-function 'ignore)
+
+;; Packages
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; use-package
+;; The use-package macro allows you to isolate package configuration in your
+;; .emacs file in a way that is both performance-oriented and, well, tidy. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; which-key 
+;; Which key is  a minor mode for Emacs that displays the key bindings
+;; following your currently entered incomplete command (a prefix) in a popup.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package which-key
+  :ensure
+  :init
+  (which-key-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; magit
+;; Magit is a complete text-based user interface to Git. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)
+         ("C-x C-g" . magit-status)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; company
+;; Company is a text completion framework for Emacs.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package company
+  :ensure t
+  :defer t
+  :init (global-company-mode)
+  :config
+  (progn
+    ;; Use Company for completion
+    (bind-key [remap completion-at-point] #'company-complete company-mode-map)
+
+    (setq company-tooltip-align-annotations t
+          ;; Easy navigation to candidates with M-<n>
+          company-show-quick-access t)
+    (setq company-dabbrev-downcase nil))
+  :diminish company-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; move-dup
+;; Minor mode for Eclipse-like moving and duplicating lines or rectangles.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package move-dup
+  :ensure
+  :bind (("M-p"   . move-dup-move-lines-up)
+         ("C-M-p" . move-dup-duplicate-up)
+         ("M-n"   . move-dup-move-lines-down)
+         ("C-M-n" . move-dup-duplicate-down)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; markdown-mode
+;; Use markdown-mode for markdown files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package markdown-mode
+  :ensure t
+  :mode (".md" ".markdown"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; linum-relative
+;; Display relative line number in Emacs.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'linum-relative)
+(linum-relative-global-mode)
+;; Use `display-line-number-mode` as linum-mode's backend for smooth performance
+(setq linum-relative-backend 'display-line-numbers-mode)
+
+;; Custom Functions
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Switch next frame.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun go-next-frame ()
+  "Switch next frame."
+  (interactive)
+  (next-window-any-frame))
+
+(defun go-previous-frame ()
+  "Switch previous frame."
+  (interactive)
+  (previous-window-any-frame))
+
+(global-set-key (kbd "M-o") 'go-next-frame)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define function to shutdown emacs server instance.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun server-shutdown ()
+  "Save buffers, Quit, and Shutdown (kill) server."
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs)
+  )
+
+;; Customizing Key Bindings 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Insert tilde
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "M-'") (lambda () (interactive) (insert "~")))
+
+
